024daad981ebf1e52f9525cb22f2693d8c6f1bdc
Oleksandr Pryhoda <pryhoda@ucu.edu.ua>
2017-04-16 12:40:51 +0300

    Add files via upload

--- Web mapping/usa_volcanoes_map.py
+++ Web mapping/usa_volcanoes_map.py
@@ -1,16 +1,25 @@
 import folium, pandas
 
+# Read data from file
 df=pandas.read_csv('Volcanoes-USA.txt')
+
+# Create a map object with some parameters
 map=folium.Map(location=[df['LAT'].mean(), df['LON'].mean()], zoom_start=5, tiles='Stamen Terrain')
 
+# Create feature group
 fg=folium.FeatureGroup('USA Volcanoes Locations')
 
+# Add to feature group all markers
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
     fg.add_child(folium.Marker([lat, lon], popup=name,
                   icon=folium.Icon(color='green' if elev in range(0, 1500)
                   else 'orange' if elev in range(1000, 3000) else 'red')))
 
+# Add feature group to map
 map.add_child(fg)
+
+# Add layer control
 map.add_child(folium.LayerControl())
 
+# Save map as html file
 map.save(outfile='usa_volcanoes.html')
--- Web mapping/world_population.py
+++ Web mapping/world_population.py
@@ -1,13 +1,18 @@
 import folium, pandas
 
+# Read data from file
 df=pandas.read_csv('Volcanoes-USA.txt')
+
+# Create a map object with some parameters
 map=folium.Map(location=[], zoom_start=2, tiles='Mapbox bright')
 
-
+# Add to map different colors depending on population size
 map.add_child(folium.GeoJson(data=open('world-geojson-from-ogr.json', encoding='utf-8-sig'),
 name='World population', style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] <= 10000000
                             else 'orange' if 10000000 < x['properties']['POP2005'] <= 20000000 else 'red'}))
 
-
+# Add layer control
 map.add_child(folium.LayerControl())
+
+# Save map as html file
 map.save('world_population.html')
